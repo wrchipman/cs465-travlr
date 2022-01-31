4712ab1670716a7c1c2282207d2c46b04290139b
William Chipman <wrchipman@gmail.com>
2022-01-30 23:11:24 -0300

    module3 complete

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,3 +1,6 @@
+var fs = require("fs");
+var trips = JSON.parse(fs.readFileSync("./data/trips.json", "utf8"));
+
 const travel = (req, res) => {
   res.render("travel", { title: "Travlr Getaways" });
 };
