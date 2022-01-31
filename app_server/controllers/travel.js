3fd7d6be028180ecb4bdc57fbdce6cdc4bf7b9b0
William Chipman <wrchipman@gmail.com>
2022-01-30 23:24:20 -0300

    Fixed typo

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -2,7 +2,7 @@ var fs = require("fs");
 var trips = JSON.parse(fs.readFileSync("./data/trips.json", "utf8"));
 
 const travel = (req, res) => {
-  res.render("travel", { title: "Travlr Getaways" });
+  res.render("travel", { title: "Travlr Getaways", trips });
 };
 module.exports = {
   travel,
